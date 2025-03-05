54867be67d3ebd6c4973c6b42f5c82158fe4998f
Cristian <cristian.bucataru@gmail.com>
2025-03-05 22:58:37 +0200

    interprocess required changes

--- tokio/src/io/mod.rs
+++ tokio/src/io/mod.rs
@@ -249,6 +249,15 @@ cfg_net_unix! {
     }
 }
 
+cfg_net! {
+    cfg_not_wasi_classic! {
+        mod async_fd;
+        pub mod wasi {
+            pub use super::async_fd::AsyncFd;
+        }
+    }
+}
+
 cfg_io_std! {
     mod stdio_common;
 
